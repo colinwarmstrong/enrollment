45f2ed76fdc7dec42b49b51f2fddd98880d293de
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-10 15:05:05 -0600

    Add courses table and StudentCourses join table to the database.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -7,5 +7,7 @@ describe Student, type: :model do
 
   describe 'relationships' do
     it{should have_many :addresses}
+    it{should have_many :student_courses}
+    it{should have_many :courses}    
   end
 end
