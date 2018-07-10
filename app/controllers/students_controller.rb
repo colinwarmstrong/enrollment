a46d6b4173857e8526a50b10b7ea4daf956a3ae2
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-10 14:10:02 -0600

    Add ability for user to edit a student. Also creates strong params for student controller.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,23 @@ class StudentsController < ApplicationController
   end
 
   def create
-    student = Student.create(name: params[:student][:name])
+    student = Student.create(student_params)
     redirect_to student_path(student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    student = Student.find(params[:id])
+    student.update(student_params)
+    redirect_to student_path(student)
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name)
   end
 end
