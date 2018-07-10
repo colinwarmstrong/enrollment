d733a88f50f9a66eb5dea6dab7ef192baf0af304
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-10 14:03:14 -0600

    Add ability for user to create a new student via a form.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,4 +11,9 @@ class StudentsController < ApplicationController
   def new
     @student = Student.new
   end
+
+  def create
+    student = Student.create(name: params[:student][:name])
+    redirect_to student_path(student)
+  end
 end
--- spec/features/students/user_can_create_a_student_spec.rb
+++ spec/features/students/user_can_create_a_student_spec.rb
@@ -4,10 +4,10 @@ describe "user visits '/students/new'" do
   it 'filling in a name and clicking submit creates a student' do
     visit new_student_path
 
-    fill_in :name, with: 'Colin'
+    fill_in 'student[name]', with: 'Colin'
 
     click_on 'Submit'
-
+    
     expect(current_path).to eq(student_path(1))
     expect(page).to have_content('Colin')
   end
